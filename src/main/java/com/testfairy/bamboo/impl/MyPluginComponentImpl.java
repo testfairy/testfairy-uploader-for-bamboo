c051fdc9b8e8ebc368513916f9dfe97d1f7e2d36
Gil Tselenchuk <giltsl@gmail.com>
2016-03-22 14:20:14 +0200

    format

--- src/main/java/com/testfairy/bamboo/impl/MyPluginComponentImpl.java
+++ src/main/java/com/testfairy/bamboo/impl/MyPluginComponentImpl.java
@@ -1,6 +1,7 @@
 package com.testfairy.bamboo.impl;
 
 import com.atlassian.bamboo.build.logger.BuildLogger;
+import com.atlassian.bamboo.configuration.ConfigurationMap;
 import com.atlassian.bamboo.task.*;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
@@ -15,88 +16,86 @@ import org.jetbrains.annotations.NotNull;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-@ExportAsService ({MyPluginComponent.class})
-@Named ("myPluginComponent")
-public class MyPluginComponentImpl implements MyPluginComponent, TaskType
-{
-    @ComponentImport
-    private final ApplicationProperties applicationProperties;
+@ExportAsService({MyPluginComponent.class})
+@Named("myPluginComponent")
+public class MyPluginComponentImpl implements MyPluginComponent, TaskType {
+        @ComponentImport
+        private final ApplicationProperties applicationProperties;
 
-    @Inject
-    public MyPluginComponentImpl(final ApplicationProperties applicationProperties)
-    {
-        this.applicationProperties = applicationProperties;
-    }
-
-    public String getName()
-    {
-        if(null != applicationProperties)
-        {
-            return "myComponent:" + applicationProperties.getDisplayName();
+        @Inject
+        public MyPluginComponentImpl(final ApplicationProperties applicationProperties) {
+                this.applicationProperties = applicationProperties;
         }
 
-        return "myComponent";
-    }
+        public String getName() {
+                if (null != applicationProperties) {
+                        return "myComponent:" + applicationProperties.getDisplayName();
+                }
 
-    @NotNull
-    public TaskResult execute(@NotNull TaskContext taskContext) throws TaskException {
+                return "myComponent";
+        }
 
-        final BuildLogger buildLogger = taskContext.getBuildLogger();
+        @NotNull
+        public TaskResult execute(@NotNull TaskContext taskContext) throws TaskException {
 
-        String API_KEY = "5f8d490c554f63cf7784174bcdcb3c87f2447709";
-        String APK_PATH = "/tmp/Ham/out/ham.apk";
+                final BuildLogger buildLogger = taskContext.getBuildLogger();
+
+                ConfigurationMap configurationMap = taskContext.getConfigurationMap();
+
+                String API_KEY = "5f8d490c554f63cf7784174bcdcb3c87f2447709";
+                String APK_PATH = "/tmp/Ham/out/ham.apk";
 //        String KEYSTORE_PATH = "/tmp/Ham/out/debug.keystore";
 //        String KEYSTORE_ALIAS = "androiddebugkey";
 //        String STORE_PASSWORD = "android";
 //        String KEY_PASSWORD = "";
 
 
-        Options options = new Options.Builder()
-                .notifyTesters(true)
-                .setComment("Uploading New Build")
-                .build();
+                Options options = new Options.Builder()
+                    .notifyTesters(true)
+                    .setComment("Uploading New Build")
+                    .build();
 
-        AndroidUploader uploader = new AndroidUploader.Builder(API_KEY)
-                .setOptions(options)
-                .setApkPath(APK_PATH)
-                .enableInstrumentation(false)
+                AndroidUploader uploader = new AndroidUploader.Builder(API_KEY)
+                    .setOptions(options)
+                    .setApkPath(APK_PATH)
+                    .enableInstrumentation(false)
 //                .setKeystore(KEYSTORE_PATH, KEYSTORE_ALIAS, STORE_PASSWORD, KEY_PASSWORD)
-                .build();
+                    .build();
 
-        uploader.upload(new Listener() {
+                uploader.upload(new Listener() {
 
-            public void onUploadStarted() {
+                        public void onUploadStarted() {
 //                System.out.println("onUploadStarted");
-                buildLogger.addBuildLogEntry("onUploadStarted");
-            }
+                                buildLogger.addBuildLogEntry("onUploadStarted");
+                        }
 
-            public void onUploadComplete(Build build) {
+                        public void onUploadComplete(Build build) {
 //                System.out.println("onUploadComplete");
 //                System.out.println(build.appName());
 
-                buildLogger.addBuildLogEntry("onUploadComplete");
+                                buildLogger.addBuildLogEntry("onUploadComplete");
 
-                buildLogger.addBuildLogEntry("App name: " +  build.appName());
-                buildLogger.addBuildLogEntry("buildUrl: " +  build.buildUrl());
+                                buildLogger.addBuildLogEntry("App name: " + build.appName());
+                                buildLogger.addBuildLogEntry("buildUrl: " + build.buildUrl());
 
-            }
+                        }
 
-            public void onUploadFailed(Throwable throwable) {
+                        public void onUploadFailed(Throwable throwable) {
 //                System.out.println("onUploadFailed");
-                buildLogger.addBuildLogEntry("onUploadFailed");
-                buildLogger.addBuildLogEntry(throwable.getMessage());
-            }
+                                buildLogger.addBuildLogEntry("onUploadFailed");
+                                buildLogger.addBuildLogEntry(throwable.getMessage());
+                        }
 
-            public void onProgress(float v) {
+                        public void onProgress(float v) {
 
-            }
-        });
+                        }
+                });
 
 
 //        final String say = taskContext.getConfigurationMap().get("say");
 
 //        buildLogger.addBuildLogEntry(say);
 
-        return TaskResultBuilder.create(taskContext).success().build();
-    }
+                return TaskResultBuilder.create(taskContext).success().build();
+        }
 }
